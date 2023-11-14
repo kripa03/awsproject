f014cb58924e3d1b42f81a15c69343545a595959
kripa03 <144186003+kripa03@users.noreply.github.com>
2023-11-13 20:17:16 -0600

    Update start_app.sh

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -1,3 +1,3 @@
 aws ecr get-login-password --region region | docker login --username AWS --password-stdin 639377170566.dkr.ecr.us-east-1.amazonaws.com
-docker pull 639377170566.dkr.ecr.us-east-1.amazonaws.com/webapp:v4
-docker run -d -p 8000:8000 639377170566.dkr.ecr.us-east-1.amazonaws.com/webapp:v4
+docker pull 639377170566.dkr.ecr.us-east-1.amazonaws.com/webapp:v1
+docker run -d -p 8000:8000 639377170566.dkr.ecr.us-east-1.amazonaws.com/webapp:v1
