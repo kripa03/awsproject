335ec0333fca9fa0fb25f68e02de14ba435845a4
kripa03 <144186003+kripa03@users.noreply.github.com>
2023-11-13 18:38:52 -0600

    123

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -1,3 +1,3 @@
-aws ecr get-login-password --region region | docker login --username AWS --password-stdin 998503830099.dkr.ecr.us-east-1.amazonaws.com
-docker pull 998503830099.dkr.ecr.us-east-1.amazonaws.com/production:v4
-docker run -d -p 8000:8000 998503830099.dkr.ecr.us-east-1.amazonaws.com/production:v4
+aws ecr get-login-password --region region | docker login --username AWS --password-stdin 639377170566.dkr.ecr.us-east-1.amazonaws.com
+docker pull 639377170566.dkr.ecr.us-east-1.amazonaws.com/webapp:v4
+docker run -d -p 8000:8000 639377170566.dkr.ecr.us-east-1.amazonaws.com/webapp:v4
